e748f747e1f07d50b77eeef26e6f9c66af94f91c
Touhid Mia <hmtm.cse@gmail.com>
2019-07-23 00:10:23 +0600

    update build script

--- scripts/build_doc.sh
+++ scripts/build_doc.sh
@@ -10,3 +10,16 @@ echo -e "---\npermalink: /404.html\n---"
 git add docs
 git commit -m "Updated at `date '+%H:%M:%S %A %d-%B, %Y'`"
 git push --all
+
+rm -rf website
+cp -a docs website
+cp README.md website
+cp LICENSE website
+cd website
+git init
+git add --all
+git commit -m "Updated at `date '+%H:%M:%S %A %d-%B, %Y'`"
+git remote add origin https://github.com/tm-react/tm-react.github.io.git
+git push origin master --force
+cd ..
+rm -rf website
